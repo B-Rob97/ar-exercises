078ca7a4d414651cd8717c58f322c5eaac85e8c1
Brett Roberts <brett.roberts@live.ca>
2023-10-17 11:31:29 -0600

    Add querires to calculate total and average revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,14 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts "Calculating Revenue from ALL Stores"
+
+@total_revenue_all_store = Store.sum("annual_revenue")
+puts "TOTAL REVENUE: #{@total_revenue_all_store}$"
+
+puts "Calculating Average Revenue of All Stores"
+
+@stores_created_count = Store.count
+
+@average_revenue = @total_revenue_all_store / @stores_created_count
+puts "AVERAGE REVENUE: #{@average_revenue}$"
