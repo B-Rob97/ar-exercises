f8e36674c0b12b36dddc3345435a2d6e5d5b5227
Brett Roberts <brett.roberts@live.ca>
2023-10-17 11:29:45 -0600

    Lint fix

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,15 +6,12 @@ puts "Exercise 3"
 puts "----------"
 
 puts "Storing store 3 into an instance variable!"
-puts "--------------------------------------"
 
 stores_created_count_1 = Store.count
-puts "There are #{stores_created_count_1} stores active"
+puts "There are #{stores_created_count_1} active stores"
 
 @store3 = Store.find(3)
 @store3.destroy
 
-
-
 stores_created_count_2 = Store.count
-puts "There are #{stores_created_count_2} stores active"
+puts "There are #{stores_created_count_2} active stores"
