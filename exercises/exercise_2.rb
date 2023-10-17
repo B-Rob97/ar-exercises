21d035c33d7110cd4082452d1bdf2112af94288d
Brett Roberts <brett.roberts@live.ca>
2023-10-17 09:00:20 -0600

    Add stores 1 & 2 into instance variables

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,14 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+puts "Assigning stores to instance variables"
+puts "--------------------------------------"
+
+@store1 = Store.find(1)
+puts @store1.name
+
+@store2 = Store.find(2)
+puts @store2.name
+
+@store1.update(name: "Calgary")
+puts @store1.name
