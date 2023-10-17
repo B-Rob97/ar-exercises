bd258b9f7865e24ab03de384712f6f9f69212480
Brett Roberts <brett.roberts@live.ca>
2023-10-17 11:31:00 -0600

    Add more stores - add queries to find mens & womans with income less than 1 million

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,38 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+surrey = Store.create(
+  name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+whistler = Store.create(
+  name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+yaletown = Store.create(
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+puts "All Mens Stores"
+puts "--------------------------------------------------"
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts store.name
+end
+puts "All Womans Store with Income LESS than 1 Million"
+puts "--------------------------------------------------"
+@womans_stores_low_income = Store.where("womens_apparel = true AND annual_revenue < 1000000")
+
+@womans_stores_low_income.each do |store|
+  puts store.name
+end
