29f105a0f047451be2b3cf91863b2fcb4bcd4131
Brett Roberts <brett.roberts@live.ca>
2023-10-17 09:00:52 -0600

    =Add logic to destroy store 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,16 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+puts "Storing store 3 into an instance variable!"
+puts "--------------------------------------"
+
+stores_created_count_1 = Store.count
+puts "There are #{stores_created_count_1} stores active"
+
+@store3 = Store.find(3)
+@store3.destroy
+
+
+
+stores_created_count_2 = Store.count
+puts "There are #{stores_created_count_2} stores active"
