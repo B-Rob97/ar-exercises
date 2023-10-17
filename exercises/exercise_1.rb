7143a9f40540fe610d4d5b4d882eec95c7ebc017
Brett Roberts <brett.roberts@live.ca>
2023-10-17 08:30:29 -0600

    Add stores seed data logic

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,29 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+puts "Creating stores :)"
+
+burnaby = Store.create(
+  name: "Burnaby",
+  annual_revenue: 300000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+richmond = Store.create(
+  name: "Richmond",
+  annual_revenue: 1260000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+gastown = Store.create(
+  name: "Gastown",
+  annual_revenue: 190000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+stores_created_count = Store.count
+
+puts "#{stores_created_count} stores created succesfully!"
