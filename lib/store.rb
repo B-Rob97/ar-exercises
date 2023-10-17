e471bbf495836b881f4bd4f5c1f5b913759d88b4
Brett Roberts <brett.roberts@live.ca>
2023-10-17 11:33:56 -0600

    Add validation

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,24 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate  :must_carry_at_least_one_apparel
+
+  private
+
+  def must_have_valid_revenue
+    if !annual_revenue
+      errors.add(:base, "Store must have valid revenue of at least 0$ or greater")
+    end
+  end
+  
+
+  def must_carry_at_least_one_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:base, "Stores must carry at least one of the men's or women's apparel")
+    end
+  end
+
 
 end
